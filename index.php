e30e300eb5d6e17669e18411f92ad317b7276850
unknown <julien.lcx@gmail.com>
2023-04-03 09:32:23 +0200

    fixphp

--- index.php
+++ index.php
@@ -30,25 +30,32 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="./styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
   </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Expo Picasso</a>
+  <div class="page">
+    <nav class="navbar navbar-expand-lg navbar-dark ">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="./index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./oeuvres.html">Les oeuvres</a>
-            </li>
-            <li class="nav-item">
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+          <a class="navbar-brand" href="#"><img class="logo" src="/img/71295.png"></img>Picasso</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="collapse navbar-collapse justify-content-start" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link text-center" href="./index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./oeuvres.html">Les oeuvres</a>
+              </li>
+              <li class="nav-item">
                 <a class="nav-link" href="./Apropos.html">A propos de Picasso</a>
               </li>
               <li class="nav-item">
@@ -57,11 +64,15 @@ try {
               <li class="nav-item">
                 <a class="nav-link" href="#">Tarifs</a>
               </li>
-          </ul>
-        </div>
+              <li class="nav-item">
+                <a class="nav-link" href="./avis.html">Avis</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </nav>
       <br>
-      <table class="table table-warning table-striped-columns">
+      <table class="table">
         <thead>
           <tr>
             <th scope="col">Catégories</th>
